1a680435573513aa12f13527e833199ef471b71a
Tạ Quang Tùng <109160074+kibo2003@users.noreply.github.com>
2023-01-07 17:53:29 +0700

    Delete Ôn tập cuối kỳ directory

--- Ôn tập cuối kỳ/BaiTap05.R
+++
@@ -1,9 +0,0 @@
-# Bài 5: Datasets sleep (gói datasets)
-# Tính thời gian ngủ tăng thêm trung bình của 2 nhóm
-sleepExtra <- sleep
-groupID <- c(levels(sleepExtra$group))
-
-sapply(groupID, function(x){
-  TBsleep = sleepExtra[sleepExtra$group == x,]
-  return (mean(TBsleep$extra))
-})
